8f29259dfc28258486364071c1c6f3643ee4add9
Andrew Laidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2025-04-01 14:01:59 +0100

    First python version with mapepire

---
+++ server.py
@@ -0,0 +1,60 @@
+from flask import Flask,  jsonify
+from flask_cors import CORS, cross_origin
+import datetime
+import os
+
+import configparser
+from mapepire_python.client.sql_job import SQLJob
+from mapepire_python.data_types import DaemonServer
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+creds = DaemonServer(
+    host=config['mapepire']['SERVER'],
+    port=config['mapepire']['PORT'],
+    user=config['mapepire']['USER'],
+    password=config['mapepire']['PASSWORD'],
+    ignoreUnauthorized=True,
+)
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+# Added root endpoint
+def root():
+    content = {}
+    content['status'] = "Good"
+    now = datetime.datetime.now()
+    content['time'] = now.strftime("%H:%M:%S")
+    return jsonify(content)
+
+@app.route('/employee/<int:empid>')
+# One employee
+def employee(empid: int):
+    with SQLJob(creds) as job:
+        with job.query('select * from SAMPLE.employee where EMPNO=' + str(empid)) as query:
+            results = query.run(rows_to_fetch=1)
+            data = results
+        return data
+
+@app.route('/getProducts')
+# All products
+def getProducts():
+    with SQLJob(creds) as job:
+        with job.query('select * from SAMPLE.product') as query:
+            results = query.run()
+        return results
+
+@app.route('/getEmployees')
+# All employees
+def getEmployees():
+    with SQLJob(creds) as job:
+        with job.query('select EMPNO,FIRSTNME,LASTNAME,JOB from SAMPLE.employee') as query:
+            results = query.run()
+        return results
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8081)
